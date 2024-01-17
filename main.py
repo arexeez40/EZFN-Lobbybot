98749414fa44e76054df4474e856ae719e609741
arexeez40 <150522470+arexeez40@users.noreply.github.com>
2024-01-17 18:41:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print('\033[94mInstalling packages, please wait...')
 os.system('pip install -U EZFNSetup >/dev/null 2>&1')
 print('Installed the packages!\033[0m')
 import EZFNSetup
+.github/workflows/python-publish.yml
